dd88595f3321cc3d26e68b91c9ae69dd5853454f
Peter Vizi <peter.vizi@gmail.com>
2008-12-24 21:35:53 -0500

    MOC support
    Some error checking.

--- src/moc.c
+++ src/moc.c
@@ -10,26 +10,64 @@
 gboolean get_moc_info(struct TrackInfo* ti) {
     char* pch;
     char temp[BUFF_SIZE];
-    FILE* pipe = popen("mocp -Q '%song;%artist;%album;%state;%ts;%cs'", "r");
+    FILE* pipe = popen("mocp -Q '%song ;%artist ;%album ;%state;%ts ;%cs ;%file ; '", "r");
+    if (!pipe) {
+	trace("No mocp");
+	return FALSE;
+    }
     fgets(temp, BUFF_SIZE, pipe);
     pclose(pipe);
     pch = strtok(temp, ";");
-    strcpy(ti->track, pch);
-    pch = strtok(NULL, ";");
-    strcpy(ti->artist, pch);
-    pch = strtok(NULL, ";");
-    strcpy(ti->album, pch);
-    pch = strtok(NULL, ";");
-    if (strcmp(pch, "STOP") == 0) {
-	ti->status = STATUS_OFF;
-    } else if (strcmp(pch, "PLAY") == 0) {
-	ti->status = STATUS_NORMAL;
-    } else if (strcmp(pch, "PAUSED") == 0) {
-	ti->status = STATUS_PAUSED;
+    if (pch != NULL) {
+	strcpy(ti->track, pch);
+    } else {
+	strcpy(ti->track, "");
     }
     pch = strtok(NULL, ";");
-    ti->totalSecs = atoi(pch);
+    if (pch != NULL) {
+	strcpy(ti->artist, pch);
+    } else {
+	strcpy(ti->artist, "");
+    }
     pch = strtok(NULL, ";");
-    ti->currentSecs = atoi(pch);
+    if (pch != NULL) {
+	strcpy(ti->album, pch);
+    } else {
+	strcpy(ti->album, "");
+    }
+    pch = strtok(NULL, ";");
+    if (pch != NULL) {
+	if (strcmp(pch, "STOP") == 0) {
+	    ti->status = STATUS_OFF;
+	} else if (strcmp(pch, "PLAY") == 0) {
+	    ti->status = STATUS_NORMAL;
+	} else if (strcmp(pch, "PAUSED") == 0) {
+	    ti->status = STATUS_PAUSED;
+	} else {
+	    ti->status = STATUS_OFF;
+	}
+    } else {
+	ti->status = STATUS_OFF;
+    }
+    pch = strtok(NULL, ";");
+    if (pch != NULL) {
+	ti->totalSecs = atoi(pch);
+    } else {
+	ti->totalSecs = 0;
+    }
+    pch = strtok(NULL, ";");
+    if (pch != NULL) {
+	ti->currentSecs = atoi(pch);
+    } else {
+	ti->currentSecs = 0;
+    }
+    // Check for internet radio
+    pch = strtok(NULL, ";");
+    if (strcmp(ti->album, " ") == 0 && strcmp(ti->artist, " ") == 0) {
+	if (strstr(pch, "http://") != NULL) {
+	    strcpy(ti->artist, pch);
+	    strcpy(ti->album, "Online Radio");
+	}
+    }
     return TRUE;
 }
